a4d547fa1235f15b3916ec448063de655e5b0bf2
Melissa Martinez <123403764+martinez415@users.noreply.github.com>
2024-08-19 03:02:38 +0000

    Updated Orca App - unit testing

--- tests/test_orca.rb
+++ tests/test_orca.rb
@@ -1,9 +1,22 @@
 # tests/test_orca.rb
 
 require 'minitest/autorun'
-require './facts'
 require './quiz'
-
+require './facts'
 class TestOrca < Minitest::Test
+  def test_question_bank
+    quiz = Quiz.new
+    questions = quiz.question_bank
+  
+    assert_instance_of Array, questions
+    assert_equal 9, questions.length
+  end
 
+  def test_lpod_info
+    fact = Facts.new
+    lpod_facts = fact.lpod_info
+  
+    assert_instance_of Array, lpod_facts
+    assert_equal 5, lpod_facts.length
+  end
 end
